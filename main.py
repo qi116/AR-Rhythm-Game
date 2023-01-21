3a8d1bb826a395c9138a7236a6de08ef8337f46b
qi116 <qibrianm@gmail.com>
2023-01-21 14:27:12 -0500

    Parse hit markers from txt file

--- main.py
+++ main.py
@@ -1,29 +1,67 @@
 import cv2
 import mediapipe as mp
 import random
+import os
+
 rectangle_frames = 0
 rectangle_coords = (200,200)
 
+screen_width = 640
+screen_height = 360
 
-def handle(results):
+class Hit:
+  def __init__(self, x, y, time): #time is in miliseconds
+    self.x = x
+    self.y = y
+    self.time = time
+  def __str__(self):
+     return "x: " + str(self.x) + " y: " + str(self.y) + " time (ms): " + str(self.time)
+def opensong(path):
+  file = open(path, 'r')
+  lines = file.readlines()
+  clicks = []
+  for line in lines:
+    lineSplit = line.split(",")
+    h = Hit(lineSplit[0], lineSplit[1], lineSplit[2])
+    clicks.append(h)
+  return clicks
+
+clicks = opensong(os.getcwd() + "\songs\photograph.txt")
+for click in clicks:
+  print(click)
+
+def handle(results, image):
   #print('here')
+  BLUE = (255, 0, 0)
+  
+  axes = 50, 50
+  angle = 0
   try: 
     leftx = (results.pose_landmarks.landmark[19].x + results.pose_landmarks.landmark[15].x) / 2
     lefty = (results.pose_landmarks.landmark[19].y + results.pose_landmarks.landmark[15].y) / 2
 
-    rightx = (results.pose_landmarks.landmark[20].x + results.pose_landmarks.landmark[16].x) / 2
+    rightx = (results.pose_landmarks.landmark[22].x + results.pose_landmarks.landmark[18].x) / 2
     righty = (results.pose_landmarks.landmark[20].y + results.pose_landmarks.landmark[16].y) / 2
-  except:
-    return
 
-  if (rightx < .5 and righty < .5):
-    print("top right - RH")
-  if (rightx < .5 and righty > .5):
-    print("bottom right - RH")
-  if (rightx > .5 and righty < .5):
-    print("top left - RH")
-  if (rightx > .5 and righty > .5):
-    print("bottom left - RH")
+    center = (int(rightx*(screen_width)),int(righty*(screen_height)))
+
+    if (rightx < .5 and righty < .5):
+      print("top right - RH")
+    if (rightx < .5 and righty > .5):
+      print("bottom right - RH")
+    if (rightx > .5 and righty < .5):
+      print("top left - RH")
+    if (rightx > .5 and righty > .5):
+      print("bottom left - RH")
+
+
+    image = cv2.circle(image, center, 50, BLUE, 2)
+    return image
+  except:
+    center = (0,0)
+    image = cv2.circle(image, center, 50, BLUE, 2)
+    return image
+
 
   if (leftx < .5 and lefty < .5):
     print("top right - LH")
@@ -49,6 +87,8 @@ with mp_pose.Pose(
     min_tracking_confidence=0.5) as pose:
   while cap.isOpened():
     success, image = cap.read()
+    image = cv2.resize(image, (screen_width,screen_height), interpolation =cv2.INTER_AREA)
+
     if not success:
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
@@ -61,7 +101,7 @@ with mp_pose.Pose(
     results = pose.process(image)
 
     #print(results.pose_landmarks.landmark[17].y)
-    handle(results)
+    image = handle(results, image)
     # Draw the pose annotation on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -71,11 +111,10 @@ with mp_pose.Pose(
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
     # Flip the image horizontally for a selfie-view display.
-    image = cv2.resize(image, (1280,720), interpolation =cv2.INTER_AREA)
     if rectangle_frames == 0:
       if random.randint(0,60) > 55:
         rectangle_frames = 20
-        rectangle_coords = (int(random.random()*(1280-100)),int(random.random()*(720-100)))
+        rectangle_coords = (int(random.random()*(screen_width-100)),int(random.random()*(screen_height-100)))
     else:
       rectangle_frames-=1
       image = cv2.rectangle(image, rectangle_coords, (rectangle_coords[0]+100,rectangle_coords[1]+100), (255,0,0), 7)

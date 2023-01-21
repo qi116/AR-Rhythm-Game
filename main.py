e842f677f8c6efcf1f44cca2ecc2ca99c6529953
dtrager02 <dtrager02@gmail.com>
2023-01-21 00:00:13 -0500

    hit detection failed

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import random
 rectangle_frames = 0
 rectangle_coords = (200,200)
 
+def detect_hit(landmark,rectangle_coords1,rectangle_coords2):
+  normalized_x1 = rectangle_coords1[0]/1280.0
+  normalized_y1 = rectangle_coords1[1]/720.0
+  normalized_x2 = rectangle_coords2[0]/1280.0
+  normalized_y2 = rectangle_coords2[1]/720.0
+  return normalized_x1 < landmark[17].x < normalized_x2 and normalized_y1 < landmark[17].y < normalized_y2
 
 def handle(results):
   #print('here')
@@ -46,6 +52,7 @@ with mp_pose.Pose(
     min_tracking_confidence=0.5) as pose:
   while cap.isOpened():
     success, image = cap.read()
+    image = cv2.resize(image, (1280,720), interpolation =cv2.INTER_AREA)
     if not success:
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
@@ -68,14 +75,19 @@ with mp_pose.Pose(
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
     # Flip the image horizontally for a selfie-view display.
-    image = cv2.resize(image, (1280,720), interpolation =cv2.INTER_AREA)
     if rectangle_frames == 0:
       if random.randint(0,60) > 55:
-        rectangle_frames = 20
+        rectangle_frames = 100
         rectangle_coords = (int(random.random()*(1280-100)),int(random.random()*(720-100)))
+        rectangle_coords2 = (rectangle_coords[0]+100,rectangle_coords[1]+100)
     else:
-      rectangle_frames-=1
-      image = cv2.rectangle(image, rectangle_coords, (rectangle_coords[0]+100,rectangle_coords[1]+100), (255,0,0), 7)
+      print("rectangle:",rectangle_coords)
+      image = cv2.rectangle(image, rectangle_coords, rectangle_coords2, (255,0,0), 7)
+      hit = detect_hit(results.pose_landmarks.landmark,rectangle_coords,rectangle_coords2)
+      if hit:
+        rectangle_frames = 0
+        print("hit")
+      rectangle_frames-=1 
     cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
